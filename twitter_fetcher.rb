8c071b78dee62f1ae2ece909b79a7966c3460c84
tobamese <me@lizlara.com>
2013-06-22 03:23:33 -0300

    Update twitter_fetcher.rb
    added keys

--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -7,10 +7,10 @@ class TwitterFetcher < Sinatra::Base
   helpers Sinatra::Jsonp
 
   @@twitter_client = Twitter::Client.new(
-    :consumer_key       => 'YOUR-CONSUMER-KEY-JUNX',
-    :consumer_secret    => 'YOUR-CONSUMER-SECRET-JUNX',
-    :oauth_token        => 'YOUR-OAUTH-TOKEN-JUNX',
-    :oauth_token_secret => 'YOUR-OAUTH-TOKEN-SECRET-JUNX',
+    :consumer_key       => 'Xne96VDhbghcGKjOlx4hXQ',
+    :consumer_secret    => 'JYal9zB8eHFzRBPn2zsOKeg0TYPo3RUXnyuHclpWHA',
+    :oauth_token        => '38320634-DIMp3eaSl9e1vQJM2UjJBdBkeEnvVR1iHEgX8yWM',
+    :oauth_token_secret => 'ftXE4cN7RF1tUCUYRO6MCUDX2iypBbDLyQWaZeNXzQ',
   )
 
   get '/' do
